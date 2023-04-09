72a3a4cc20c90dda707de2cbfedeff78cf9cb746
ArneJohnson <arnejohnson02@gmail.com>
2023-04-09 13:36:08 +0200

    task display

--- todolist.js
+++ todolist.js
@@ -2,16 +2,36 @@ const contentElement = document.getElementById("content");
 const loginElement = document.getElementById("login");
 const taskWriterElement = document.getElementById("taskwriter");
 let currentUser = "Arne";
+let tasks = [];
+
+const task1 = {
+  title: "taskname",
+  isCompleted: false,
+};
+
+tasks.push(task1);
 
 function displayTasks() {
-  let tasks = JSON.parse(localStorage.getItem(currentUser));
-
   contentElement.innerHTML = "";
+  // let tasks = JSON.parse(localStorage.getItem(currentUser));
 
-  for (let task in tasks) {
-    const taskElement = createTaskElement(task);
+  for (let i = 0; i < tasks.length; i++) {
+    const taskElement = createTaskElement(tasks[i]);
     contentElement.appendChild(taskElement);
   }
 }
 
-function createTaskElement(task) {}
+function createTaskElement(task) {
+  const articleElement = document.createElement("article");
+
+  const taskNameElement = document.createElement("div");
+  articleElement.appendChild(taskNameElement);
+
+  const titleElement = document.createElement("p");
+  titleElement.innerText = task.title;
+  taskNameElement.appendChild(titleElement);
+
+  return articleElement;
+}
+
+displayTasks();
